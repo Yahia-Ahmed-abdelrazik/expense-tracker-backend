a0c2c9407292d398c7f115813029b4a7999e6302
Yahia-Ahmed-abdelrazik <otakayahia@gmail.com>
2025-08-31 12:54:39 +0300

    export app

--- server.js
+++ server.js
@@ -40,7 +40,9 @@ app.use("/api/v1/dashbord", dashbordRoutes);
 //server uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+export default app;
